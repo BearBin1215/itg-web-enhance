7dfb37560b82ec219f76b7671a98e04cd2f3494c
bearbin1215 <20369414+BearBin1215@users.noreply.github.com>
2024-03-29 11:03:36 +0800

    fix: OA表格粘贴实际不生效问题

--- src/oa.ts
+++ src/oa.ts
@@ -5,35 +5,38 @@
 import { pasteText } from '@/utils/clipboard';
 
 const oa = () => {
-  document.body.addEventListener('paste', async (event) => {
-    const target = event.target as EventTarget & HTMLInputElement;
-    if (!target) {
+  if (!location.pathname.includes('/workflow/')) {
+    return;
+  }
+  const interval = setInterval(() => {
+    if (!$) {
       return;
     }
-    const el = target.closest('#oTable0 tbody input.wf-input');
-    if (!el) {
-      return;
-    }
-    const text = await pasteText();
-    if (text.indexOf('\t') > -1 || text.indexOf('\n') > -1 && text.indexOf('\n') !== text.length - 1) {
-      const data = text.split('\n').map((item) => item.split('\t'));
-      const row = target.closest('tr')!;
-      const col = target.closest('td')!;
-      const rowIndex = [...row.parentNode!.children].indexOf(row);
-      const colIndex = [...col.parentNode!.children].indexOf(col);
-      data.forEach((rowValue, i) => {
-        rowValue.forEach((colValue, j) => {
-          (document.querySelector('#oTable0 tbody')!
-            .children[i + rowIndex]
-            .children[j + colIndex]
-            .querySelector(`:scope input`)! as HTMLInputElement
-          ).value = colValue;
+    clearInterval(interval);
+    document.body.addEventListener('paste', async (event) => {
+      const target = event.target as EventTarget & HTMLInputElement;
+      if (!target) {
+        return;
+      }
+      const el = target.closest('#oTable0 tbody input.wf-input');
+      if (!el) {
+        return;
+      }
+      const text = await pasteText();
+      if (text.indexOf("\t") > -1 || text.indexOf("\n") > -1 && text.indexOf("\n") !== text.length - 1) {
+        const data = text.split("\n").map((item) => item.split("\t"));
+        const rowIndex = $(target).closest("tr").index();
+        const colIndex = $(target).closest("td").index();
+        data.forEach((rowValue, i) => {
+          rowValue.forEach((colValue, j) => {
+            $("#oTable0 tbody").children().eq(i + rowIndex).children().eq(j + colIndex).find("input").val(colValue);
+          });
         });
-      });
-    } else {
-      target.value = text;
-    }
-  });
+      } else {
+        $(target).val(text);
+      }
+    });
+  }, 500);
 };
 
 export default oa;
